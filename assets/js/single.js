d6d51e730dceeebdb0728b7aaa30aa4258ee0939
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-01-17 13:55:44 -0500

    call back to homepadge if repo name does not exist.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,22 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    //grab reop name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split('=')[1];
+    console.log(repoName);
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textcontent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        //if no repo name given, redirect to the homepage
+        document.location.replace('./index.html');
+    }
+};
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -20,8 +36,8 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+        // if not successful, redirect to homepage
+      document.location.replace('./index.html');
     }
   });
 };
@@ -81,4 +97,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName()
